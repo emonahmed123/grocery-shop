e8c36f7b3b74407aac44a378faa75a0e2edd633c
Emon Ahmed <imonshomon@gmail.com>
2025-10-06 16:10:49 +0600

    change login layout

--- src/components/modal/CustomModel.tsx
+++ src/components/modal/CustomModel.tsx
@@ -20,7 +20,7 @@ const CustomModel = ({ children }) => {
     <dialog
       ref={modalRef}
       onClose={onHide}
-      className="shadow-teal-700 shadow-md border border-teal-600  p-2 rounded-md dark:bg-black dark:bg-opacity-95 dark:text-gray-100 w-full "
+      className=" shadow-md   rounded-md dark:bg-black dark:bg-opacity-95 dark:text-gray-100 w-full "
     >
       {children}
     </dialog>
